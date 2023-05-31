2180e727a1183431a72a04f9734169705bef5f2c
Hasibur Rahman <hasibur@apx_managed.vanilla>
2023-05-31 09:27:27 +0600

    Add solve

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ECE Assignments</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/hasiburdev/ece-assignments",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/hasiburdev/ece-assignments",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "ECE Assignments",
   },
-}
+};
 
-export default config
+export default config;
